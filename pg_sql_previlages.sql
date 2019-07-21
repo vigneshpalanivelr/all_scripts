11edd881ee45652a27b31bf67b46e578825df608
vigneshpalanivelr <vignesh.palanivelr@gmail.com>
2019-07-21 14:43:51 +0000

    Added FUNCTION for database_privs

--- pg_sql_previlages.sql
+++ pg_sql_previlages.sql
@@ -8,4 +8,20 @@ CREATE OR REPLACE VIEW show_functions AS
 		AND specific_schema NOT IN ('pg_catalog','information_schema');
 
 
+--FUNCTION	: To find database level previlages for provided user
+--USAGE		: SELECT * FROM database_privs('<user_name>');
+--EXAMPLE	: SELECT * FROM database_privs('postgres');
+CREATE OR REPLACE FUNCTION database_privs(text) RETURNS TABLE(username text,dbname name,PRIVILEGES  text[]) AS
+$$
+SELECT $1, datname, ARRAY(SELECT privs FROM UNNEST(ARRAY[
+	(CASE WHEN has_database_privilege($1,c.oid,'CONNECT') THEN 'CONNECT' ELSE NULL END),
+	(CASE WHEN has_database_privilege($1,c.oid,'CREATE') THEN 'CREATE' ELSE NULL END),
+	(CASE WHEN has_database_privilege($1,c.oid,'TEMPORARY') THEN 'TEMPORARY' ELSE NULL END),
+	(CASE WHEN has_database_privilege($1,c.oid,'TEMP') THEN 'CONNECT' ELSE NULL END)])foo(privs) WHERE privs IS NOT NULL) 
+FROM pg_database c 
+WHERE  has_database_privilege($1,c.oid,'CONNECT,CREATE,TEMPORARY,TEMP') 
+	AND datname NOT IN ('template0','template1');
+$$ LANGUAGE SQL;
+
+
 
