1a8e6e8f67439f205b6267f08790d1255ea06409
vigneshpalanivelr <49339526+vigneshpalanivelr@users.noreply.github.com>
2020-03-03 00:00:50 +0530

    Created logger.py

---
+++ python/pySetenv/logger.py
@@ -0,0 +1,26 @@
+import logging
+
+def setupLogger(loggerName, logFile, loglevel=logging.INFO)
+  # Create Logger
+  logger = logging.getLogger(loggerName)
+  logger.setlevel(loglevel)
+  
+  # Create Formatter
+  formatter = logging.Formatter('[%(asctime)s] - %(levelname)s - %(message)', datefmt='%Y-%m-%d %H:%M:%S %Z')
+  
+  # Create file handler which logs even DEBUG messages
+  fileHandler = logging.FileHandler(logFile, mode='w')
+  fileHandler.setLevel(logging.DEBUG)
+  fileHandler.setFormatter(formatter)
+  
+  # Create stream handler which logs even DEBUG messages
+  streamHandler = logging.StreamHandler()
+  streamHandler.setLevel(logging.DEBUG)
+  streamHandler.setFormatter(formatter)
+  
+  # Add Handlers to Logger
+  logger.addHandler(fileHandler)
+  logger.addHandler(streamHandler)
+  
+  # Return logger
+  return logger
