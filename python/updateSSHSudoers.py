97a1f7b8d877c002afa93ad55072256e474c8edb
vigneshpalanivelr <vignesh.palanivelr@gmail.com>
2020-03-11 23:54:43 +0530

    Commented print sys.path

--- python/updateSSHSudoers.py
+++ python/updateSSHSudoers.py
@@ -9,7 +9,7 @@ import fileinput
 # sys.path.append(os.path.dirname('/var/lib/jenkins/workspace/playbook-provisioning-job/all_scripts/python/pySetenv'))
 sys.path.append(os.path.dirname(os.path.realpath(__file__))+'/pySetenv/variables')
 sys.path.append(os.path.dirname(os.path.realpath(__file__))+'/pySetenv/packages')
-print sys.path
+#print sys.path
 import userdata_template
 import userdata_multipart
 
