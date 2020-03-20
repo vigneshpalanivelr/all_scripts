7b1f56cae153af27ee91430aec1d24819cac4aeb
vigneshpalanivelr <vignesh.palanivelr@gmail.com>
2020-03-20 18:23:25 +0000

    Created Ansible: Cloud-Watch Installation

--- python/pySetenv/packages/userdata_multipart.py
+++ python/pySetenv/packages/userdata_multipart.py
@@ -14,23 +14,26 @@ def get_cloud_config_data(URL):
 	
         # Check the status code
         # Add new line to the end of each line
-        if request_status.status_code == 200:
-                for line in request_status.iter_lines():
-                        data_str += line + '\n'
-                message = email.message_from_string(data_str)
-	
-	# Use Mail and YAML modules to convert the data
-        # Walking through message
-        # Confirm the content type
-        for each_part in message.walk():
-                if each_part.get_content_maintype() in ['multipart','text']:
-                        if each_part.get_content_subtype() in ['cloud-config','plain']:
-                                result_data = each_part.get_payload(decode=True)
-				print result_data 
-                                try:
-                                        data = yaml.load(result_data,yaml.SafeLoader)
-                                        return data
-                                except yaml.YAMLError as yaml_exception:
-                                        print yaml_exception
-                else:
-                        print "unable to get User-Data"
+	try:
+		if request_status.status_code == 200:
+			for line in request_status.iter_lines():
+				data_str += line + '\n'
+			message = email.message_from_string(data_str)
+
+		# Use Mail and YAML modules to convert the data
+		# Walking through message
+		# Confirm the content type
+		for each_part in message.walk():
+			if each_part.get_content_maintype() in ['multipart','text']:
+				if each_part.get_content_subtype() in ['cloud-config','plain']:
+					result_data = each_part.get_payload(decode=True)
+					print result_data 
+					try:
+						data = yaml.load(result_data,yaml.SafeLoader)
+						return data
+					except yaml.YAMLError as yaml_exception:
+						print yaml_exception
+			else:
+				print "unable to get User-Data"
+	except Exception as Error:
+		print "User is Empty : Please add some user data......"
