81826e8f3eb74cb37071fac0180660ef89a84ec5
vigneshpalanivelr <vignesh.palanivelr@gmail.com>
2020-03-22 19:39:50 +0000

    Updated PGSQL | CloudInit | EPEL | Services

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Scripts Usage
+- Ansible Gather Facts
+        ```
+        - ansible hostgroup -m setup
+        ```
+
 - Configuration file for Ansible
 	```
 	- /etc/ansible/ansible.cfg
@@ -13,9 +18,9 @@
 
 - Manual Commands to execute
 	```
-    - ansible-playbook site.yml --extra-vars "ins_pgsql=pgsql_install" --tags=pgsql_install
-    - ansible-playbook site.yml --extra-vars "ins_pgsql=pgsql_install uin_pgsql=pgsql_uninstall" --tags=pgsql_install --tags=pgsql_uninstall
-    
+    - ansible-playbook site.yml -i inventory --extra-vars "ins_pgsql=pgsql_install PG_MAJOR=9.6 PG_MINOR=6" --tags=pgsql_install
+    - ansible-playbook site.yml -i inventory --extra-vars "uin_pgsql=pgsql_uninstall PG_MAJOR=9.6 PG_MINOR=6" --tags=pgsql_uninstall
+
     - ansible-playbook site.yml -i inventory --extra-vars "ins_all=list_install" --tags=list_install
     - ansible-playbook site.yml -i inventory --extra-vars "uin_all=list_uninstall" --tags=list_uninstall
 
@@ -32,7 +37,8 @@
     - ansible-playbook site.yml -i inventory --extra-vars "rem_cw=remove_cw" --tags=remove_cw
 
     - ansible-playbook site.yml --extra-vars "mount=mount_volumes" --tags=mount_volumes
-    - ansible-playbook site.yml --extra-vars "set_ci=setup_cloud_init" --tags=setup_cloud_init
+    - ansible-playbook site.yml -i inventory --extra-vars "set_epel=set_epel_repo RHEL=7" --tags=enable_epel_repo
+    - ansible-playbook site.yml -i inventory --extra-vars "set_ci=setup_cloud_init RHEL=7" --tags=setup_cloud_init
 
     - ansible-playbook site.yml -i inventory --extra-vars "group_name=root_group cre_grp=create_group" --tags=create_group
     - ansible-playbook site.yml -i inventory --extra-vars "username=vignesh password=vignesh group_name=root_group tag_group=yes cre_usr=create_user userComment='Root User'" --tags=create_user
--- python/pySetenv/packages/userdata_multipart.py
+++ python/pySetenv/packages/userdata_multipart.py
@@ -11,29 +11,37 @@ def get_cloud_config_data(URL):
         # Try reaching the URL
         data_str  = ''
         request_status = requests.get(URL)
-	
+
         # Check the status code
         # Add new line to the end of each line
-	try:
-		if request_status.status_code == 200:
-			for line in request_status.iter_lines():
-				data_str += line + '\n'
-			message = email.message_from_string(data_str)
+        if request_status.status_code == 200:
+                for line in request_status.iter_lines():
+                        data_str += line + '\n'
+                message = email.message_from_string(data_str)
 
-		# Use Mail and YAML modules to convert the data
-		# Walking through message
-		# Confirm the content type
-		for each_part in message.walk():
-			if each_part.get_content_maintype() in ['multipart','text']:
-				if each_part.get_content_subtype() in ['cloud-config','plain']:
-					result_data = each_part.get_payload(decode=True)
-					print result_data 
-					try:
-						data = yaml.load(result_data,yaml.SafeLoader)
-						return data
-					except yaml.YAMLError as yaml_exception:
-						print yaml_exception
-			else:
-				print "unable to get User-Data"
-	except Exception as Error:
-		print "User is Empty : Please add some user data......"
+                # Use Mail and YAML modules to convert the data
+                # Walking through message
+                # Confirm the content type
+                for each_part in message.walk():
+                        if each_part.get_content_maintype() in ['multipart','text']:
+                                if each_part.get_content_subtype() in ['cloud-config','plain']:
+                                        result_data = each_part.get_payload(decode=True)
+                                        print result_data
+                                        try:
+                                                data = yaml.load(result_data,yaml.SafeLoader)
+                                                return data
+                                        except yaml.YAMLError as yaml_exception:
+                                                raise ImportError(yaml_exception)
+        else:
+                raise ImportError("Userdata is Empty : Please add some userdata......")
+
+if __name__ == '__main__':
+        parser = argparse.ArgumentParser(description='Script to Update ssh and sudoers from User-Data')
+
+        parser.add_argument('URL'               ,action='store_const' ,const='http://169.254.169.254/latest/user-data'  ,help='URL for User-Data')
+
+        # arguments     = parser.parse_args(['http://169.254.169.254/latest/user-data'])
+        arguments       = parser.parse_args()
+        URL             = arguments.URL
+
+        print get_cloud_config_data(URL)
