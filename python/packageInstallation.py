e59623bddce7f0a17c58b5f3b536cf429b796b24
root <root@ip-172-31-37-37.ap-south-1.compute.internal>
2020-03-27 09:01:41 +0000

    Updated packageInstallation.py

--- python/packageInstallation.py
+++ python/packageInstallation.py
@@ -27,6 +27,7 @@ class packageInstalation(object):
 	def __init__(self):
 		# YumBase instance is the base class that houses methods and objects required to perform all the package management functions using yum.
 		# For more : http://yum.baseurl.org/download/docs/yum-api/3.2.27/yum.YumBase-class.html#_delRepos
+		# Ref      : https://linuxhint.com/yum_centos_python/
 		execLog.debug('Object  - Created Class Object : {}')
 		try:
 			self.yBase 						= yum.YumBase()
@@ -34,7 +35,7 @@ class packageInstalation(object):
 			self.yBase.preconf.errorlevel	= 0
 			execLog.info('YUM Base SetUP  : {}'.format('YUM Base SetUp Completed'))
 		except Exception as YumBaseError:
-			execLog.error(YumBaseError.__class__.__name__ + ' ' + str(YumBaseError).rstrip())
+			execLog.error('Yum Base Object : '+YumBaseError.__class__.__name__ + ' ' + str(YumBaseError).rstrip())
 			exit(100)
 	
 	def CleanUp(self):
@@ -49,7 +50,7 @@ class packageInstalation(object):
 			execLog.info('YUM CleanUP PKG : {}'.format(self.yBase.cleanPackages()[1][0]))
 			execLog.info('YUM ChacheSetUp : {}'.format(self.yBase.setCacheDir(force=True)))
 		except Exception as YumBaseError:
-			execLog.error(YumBaseError.__class__.__name__ + ' ' + str(YumBaseError).rstrip())
+			execLog.error('Yum Base CleanUp : '+YumBaseError.__class__.__name__ + ' ' + str(YumBaseError).rstrip())
 			exit(200)
 	
 	def generatePrettyTables(self, nestedLists,fieldNames):
@@ -63,14 +64,13 @@ class packageInstalation(object):
 			execLog.info('Generated Pakgs : PrettyTable')
 			return PrettyTable
 		except Exception as generatePrettyTablesError:
-			execLog.error(generatePrettyTablesError.__class__.__name__ + ' ' + str(generatePrettyTablesError).rstrip())
+			execLog.error('Generate Pretty Table : '+generatePrettyTablesError.__class__.__name__ + ' ' + str(generatePrettyTablesError).rstrip())
 			exit(300)
 	
 	def installedPackagesInServer(self):
 		try:
 			execLog.info('YUM CleanUp     : In Progress...')
 			self.CleanUp()
-			# self.pkgDetails 	= []
 			fieldNames			= ["NAME", "VERSION", "RELEASE", "ARCHETECTURE"]
 			
 			self.packages		= self.yBase.doPackageLists()
@@ -81,7 +81,7 @@ class packageInstalation(object):
 			execLog.info('YUM CleanUp     : Completed')
 			return True
 		except Exception as PrettyTableCallInstalled:
-			execLog.error(PrettyTableCallInstalled.__class__.__name__ + ' ' + str(PrettyTableCallInstalled).rstrip())
+			execLog.error('Pretty Table : '+PrettyTableCallInstalled.__class__.__name__ + ' ' + str(PrettyTableCallInstalled).rstrip())
 			exit(400)
 	
 	def pkgCheck(self, pkg2Install):
@@ -93,31 +93,33 @@ class packageInstalation(object):
 			raise ValueError('Accepts only Dict or Str values')
 	
 	def installPakgsDict(self, pkg2Install):
-		try:
-			for key,value in pkg2Install.iteritems():
-				self.installation(value['name'], version=value['version'])
-		except Exception as pkgInstallationError:
-			execLog.error(pkgInstallationError.__class__.__name__ + ' ' + str(pkgInstallationError).rstrip())
-			exit(500)
+		for key,value in pkg2Install.iteritems():
+			self.installation(value['name'], version=value['version'])
 	
 	def installation(self, package, version=None):
-		try: 
+		try:
+			start		= False
+			install		= self.yBase.install(name=package, version=version)
+			resolveDep	= self.yBase.resolveDeps()
+			self.yBase.buildTransaction()
+			if install:
+				execLog.info('YUM Package Name : {} \tVersion : {} \tState : {}'.format(install[0].name, install[0].version, 'Installing...'))
+			else:
+				execLog.warning('YUM Package Name : {} \tVersion : {} \tState : {}'.format(package, self.pkgCache[package]['version'],'AlreadyInstalled...!'))
+			start = True
+		except yum.Errors.InstallError as InstallationError:
+			execLog.error(InstallationError.__class__.__name__ +' '+str(InstallationError).rstrip()+' '+ package+' v'+str(version))
+		if start is True:
 			try:
-				install		= self.yBase.install(name=package, version=version)
-				resolveDep	= self.yBase.resolveDeps()
-				self.yBase.buildTransaction()
+				# Auto approval and Real Installation Takes Place
+				self.yBase.conf.assumeyes = True
+				self.yBase.processTransaction()
 				if install:
-					execLog.info('YUM Package Name : {} \tVersion : {} \tState : {}'.format(install[0].name, install[0].version, 'Installing...'))
-				else:
-					execLog.warning('YUM Package Name : {} \tVersion : {} \tState : {}'.format(package, self.pkgCache[package]['version'],'AlreadyInstalled...!'))
-			except yum.Errors.InstallError as pkgInstallationError:
-				execLog.error(pkgInstallationError.__class__.__name__ + ' ' +str(pkgInstallationError).rstrip() + ' ' + package + ' ' + str(version))
-			# Auto approval and Real Installation Takes Place
-			self.yBase.conf.assumeyes = True
-			self.yBase.processTransaction()
-		except Exception as Error:
-			execLog.error(Error.__class__.__name__ + ' ' + str(pkgInstallationError).rstrip())
-			exit(500)
+					execLog.info('YUM Package Name : {} \tVersion : {} \tState : {}'.format(install[0].name, install[0].version, 'Completed'))
+				self.yBase.closeRpmDB()
+			except Exception as Error:
+				execLog.error('Process Transaction : '+Error.__class__.__name__ +' '+str(Error).rstrip())
+				exit(500)
 
 if __name__ == '__main__':
 	
@@ -151,14 +153,8 @@ if __name__ == '__main__':
 	pkgInstalled 	= packageInstalation()
 	
 	# Execution
-	try:
-		if pkgInstalled.installedPackagesInServer():
-			pkgInstalled.pkgCheck(variables['common_pks'])
-			if custome_install:
-				for i in custome_packages:
-					pkgInstalled.pkgCheck(i)
-	except Exception as installedPackages:
-		execLog.error( installedPackages.__class__.__name__ + ' ' + str(installedPackages).rstrip())
-		exit(1)
-
-
+	if pkgInstalled.installedPackagesInServer():
+		pkgInstalled.pkgCheck(variables['common_pks'])
+		if custome_install:
+			for i in custome_packages:
+				pkgInstalled.pkgCheck(i)
